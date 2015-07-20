e9b2207f611af0001aee7fd96fdf77ebd7a340cf
Vladimir Bauer <vbauerster@gmail.com>
2015-07-20 20:35:00 +0300

    defaultProps

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
+
+     /**
+      * babel claims property initializers can be defined using ES7, like:
+      *
+      *   static defaultProps = {
+      *       name: 'World'
+      *   }
+      *
+      * but, in fact it does't work at the moment
+      * see: http://babeljs.io/blog/2015/06/07/react-on-es6-plus/
+      */
+
     render() {
-        return <p>Hello World</p>;
+        var name = this.props.name;
+        return (
+            <p>Hello {name}</p>
+        );
     }
 }
+
+App.propTypes = {
+    name: React.PropTypes.string
+};
+
+App.defaultProps = {
+    name: 'World'
+};
 
 React.render(<App />, document.getElementById('app'));
